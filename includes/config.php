c04cb9cf8a9087a00251fe4d56dd4c9877e7af99
Jericho Jade Madolid <saserdote.jerichojade@gmail.com>
2021-03-12 14:14:47 +0800

    added remote database

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,8 @@
 <?php
-    $db_ServerName = "localhost";
-    $db_Username = "root";
-    $db_Password = "";
-    $db_Name = "elemgs";
+    $db_ServerName = "remotemysql.com";
+    $db_Username = "CcWWemzVNu";
+    $db_Password = "s5efYFNsIR";
+    $db_Name = "CcWWemzVNu";
 
     $conn = mysqli_connect($db_ServerName, $db_Username, $db_Password, $db_Name);
     
